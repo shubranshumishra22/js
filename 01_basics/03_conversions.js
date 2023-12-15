3496552e92b9336bb35379e4b961bb2344f0153c
Shubranshu Shekhar <137818897+shubranshumishra22@users.noreply.github.com>
2023-12-15 05:26:08 +0000

    conversionsdone

---
+++ 01_basics/03_conversions.js
@@ -0,0 +1,10 @@
+let number = 3
+let newnumber = -number
+//console.log(newnumber);
+let stra = "shub"
+let strb = "om"
+let strc = stra + strb
+//console.log(strc)
+//console.log("1" + 2 + 3)
+//console.log(1 + 2 + "3") //priority of first term is string or number
+console.log("2" === 2) //here it wont convert "2" which is string to a number bydifault, known as strict check
