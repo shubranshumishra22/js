18fce97b14a8ef602c44f3f64340f438de416bcb
Shubranshu Shekhar <137818897+shubranshumishra22@users.noreply.github.com>
2023-12-16 08:26:01 +0000

    completed

---
+++ 01_basics/05_string.js
@@ -0,0 +1,13 @@
+const name = "shubranshu"
+const dept = "ece" 
+
+//console.log('hello my name is ${name} and im from ${dept} department');
+
+let myName = "shub"
+//console.log(myName.indexOf('s'))
+
+let newName = myName.slice(1,3)
+console.log(newName)
+
+let altName = myName.substring(0, 2)
+console.log(altName)
--- 01_basics/datatypes-rev.js
+++ 01_basics/datatypes-rev.js
@@ -6,19 +6,4 @@ const Id = Symbol('123')
 const newId = Symbol('123')
 //console.log(Id === newId)
 
-//stack = copy, heap = reference 
-let name = "shub"
-let newname = name
-newname = "om"
-console.log(name)
-console.log(newname)
 
-let userOne = {
-    email : "shub@gmail.com"
-}
-
-let userTwo = userOne
-
-userTwo.email = "om.gmail.com"
-console.log(userOne.email)
-console.log(userTwo.email)
