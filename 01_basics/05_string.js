650cf93bc64e02285bfa9725b04f988a2334eb92
Shubranshu Shekhar <137818897+shubranshumishra22@users.noreply.github.com>
2023-12-19 17:51:45 +0000

    finished

--- 01_basics/05_string.js
+++ 01_basics/05_string.js
@@ -3,11 +3,17 @@ const dept = "ece"
 
 //console.log('hello my name is ${name} and im from ${dept} department');
 
-let myName = "shub"
+let myName = "  shub  "
 //console.log(myName.indexOf('s'))
 
 let newName = myName.slice(1,3)
-console.log(newName)
+//console.log(newName)
 
 let altName = myName.substring(0, 2)
-console.log(altName)
+//console.log(altName)
+//console.log(altName.trim())
+
+const url = "https://shubranshu.mishra22%20"
+
+console .log(url.replace('%20', '-'))
+console .log(url.includes("shubranshu")) //to check value is there or not
