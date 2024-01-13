c5e9b7a4b575ab4210d2496a17c8fe1cbe77204d
Shubranshu Shekhar <137818897+shubranshumishra22@users.noreply.github.com>
2024-01-13 15:50:10 +0000

    half part completed

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,14 @@
+//1.sinleton ex - object.create - constructer and 2.literals
+let sym = Symbol("key1")
+let User = 
+
+{
+    name : "shub", 
+    age : 21, 
+    [sym] : "mykey1", //important syntax : symbol as key
+    email  : "shubranshu@gmail.com"
+}
+console.log(User["email"])
+//for symbol also use [], 
+console.log(User[sym])
+Object.freeze(User) //User details gets saved
