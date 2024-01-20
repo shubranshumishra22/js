ca3a90264c1a4f08bebacdc36429999fa01135a3
Shubranshu Shekhar <137818897+shubranshumishra22@users.noreply.github.com>
2024-01-20 05:30:06 +0000

    objts done as well as API done

---
+++ 02_basics/05_objects.js
@@ -0,0 +1,16 @@
+let course = {
+    coursename : "javascript",
+    price : "6900",
+    courseInstructor : "Hitesh"
+}
+//console.log(course)
+
+//**another way of extracting key or data from objects
+
+let {price : fees} = course //fees is the new name given toit solves big complex names problem
+//console.log(fees)
+//JSON
+//API can be in objects or in arrays but both keys and values will be in string fromat
+//so first convert the API into nromal Array or Oject and then use it
+//importnt website to understand any API : json.formatter.org
+ 
